0f5d319d1bb04dc3670112132dbe61a958c77f95
RA354347 <RA354347@wipro.com>
2019-04-07 23:51:25 +0530

    implemented get endpoints to fetch users and tasks

--- src/index.js
+++ src/index.js
@@ -18,13 +18,50 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/users', (req, res) => {
+    User.find({}).then(users => {
+        res.send(users);
+    }).catch(error => {
+        res.status(500).send(error);
+    })
+});
+
+app.get('/users/:id', (req, res) => {
+    User.findById(req.params.id).then(user => {
+        if( !user ) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    }).catch(error => {
+        res.status(500).send(error);
+    })
+});
+
 app.post('/tasks', (req, res) => {
-    console.log(req.body);
     const task = new Task(req.body);
     task.save().then(() => {
         res.status(201).send(task);
     }).catch(error => {
         res.status(400).send(error);
+    })
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then(tasks => {
+        res.send(tasks);
+    }).catch(error => {
+        res.status(500).send(error);
+    })
+});
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id).then(task => {
+        if(!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    }).catch(error => {
+        res.status(500).send(error);
     })
 })
 
