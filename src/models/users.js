849d8d9eb0911783eb78170888a87b11d507d7cb
RA354347 <RA354347@wipro.com>
2019-04-07 20:33:00 +0530

    User model structure created

--- src/models/users.js
+++ src/models/users.js
@@ -1,34 +1,39 @@
-// const mongoose = require('../db/mongoose.js');
-// const validator = require('validator');
+const mongoose = require('../db/mongoose.js');
+const validator = require('validator');
 
-// const User = mongoose.model('User', {
-//     name : {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//         lowercase: true,
-//         validate(value) {
-//             if(value.toLowercase().includes('password')) {
-//                 console.log('Password should not contain password.');
-//             }
-//         }
-//     },
-//     age: {
-//         type: Number,
-//         default: 0,
-//         validate(value) {
-//             if(value < 0) {
-//                 console.log('Age should be positive.')
-//             }
-//         }
-//     }
-// })
+const User = mongoose.model('User', {
+    name : {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if( !validator.isEmail(value) ) {
+                throw new Error('Password is invalid');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        lowercase: true,
+        validate(value) {
+            if(value.toLowercase().includes('password')) {
+                throw new Error('Password should not contain password.');
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new error('Age must be positive.');
+            }
+        }
+    }
+});
