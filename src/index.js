87c3ca2455f4ecbe8361c1b9288b414a18d82821
RA354347 <RA354347@wipro.com>
2019-04-08 15:08:34 +0530

    implemented async await to the http requests

--- src/index.js
+++ src/index.js
@@ -3,66 +3,128 @@ require('./db/mongoose.js');
 const User = require('./models/users.js');
 const Task = require('./models/tasks.js');
 
+// Instantiating express by executing express() function.
 const app = express();
+
+// Setting the port
 const port = process.env.PORT || 3001;
 
 // It will automatically parse JSON request into Object
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
-    user.save().then(() => {
+    try {
+        await user.save();
         res.status(201).send(user);
-    }).catch(error => {
+    } catch (error) {
         res.status(400).send(error);
-    });
+    }
+
+    /* Below is the request handler with callback  */
+    /* user.save().then(() => {
+            res.status(201).send(user);
+        }).catch(error => {
+            res.status(400).send(error);
+        });
+    */
 });
 
-app.get('/users', (req, res) => {
-    User.find({}).then(users => {
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
         res.send(users);
-    }).catch(error => {
+    } catch (error) {
         res.status(500).send(error);
-    })
+    }
 });
 
-app.get('/users/:id', (req, res) => {
-    User.findById(req.params.id).then(user => {
-        if( !user ) {
+app.get('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
             return res.status(404).send();
         }
         res.send(user);
-    }).catch(error => {
+    } catch (error) {
         res.status(500).send(error);
-    })
+    }
 });
 
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body);
-    task.save().then(() => {
-        res.status(201).send(task);
-    }).catch(error => {
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = [ 'name', 'password', 'email', 'age' ];
+    const isValidRequest = updates.every( update => allowedUpdates.includes(update) );
+
+    if( !isValidRequest ) {
+        return res.status(404).send({ 'error': 'Invalid update request.' });
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body,
+            { new: true, runValidators: true });
+        if( !user ) {
+            return res.status(404).send(); 
+        }
+        res.send(user);
+    } catch(error) {
         res.status(400).send(error);
-    })
+    }
 });
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then(tasks => {
+
+
+// ==================== End points to manage tasks =====================
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    try {
+        await task.save();
+        res.status(201).send(task);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({});
         res.send(tasks);
-    }).catch(error => {
+    } catch (error) {
         res.status(500).send(error);
-    })
+    }
 });
 
-app.get('/tasks/:id', (req, res) => {
-    Task.findById(req.params.id).then(task => {
-        if(!task) {
+app.get('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) {
             return res.status(404).send();
         }
         res.send(task);
-    }).catch(error => {
+    } catch (error) {
         res.status(500).send(error);
-    })
+    }
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = [ 'description' ];
+    const isValidRequest = updates.every( update => allowedUpdates.includes(update));
+
+    if( !isValidRequest ) {
+        res.status(404).send({ 'error': 'Invalid update request.' });
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body,
+            { new: true, runValidators: true });
+        if( !task ) {
+            res.status(404).send();
+        }
+        res.send(task);
+    } catch(error) {
+        res.status(400).send(error);
+    }
 })
 
 
