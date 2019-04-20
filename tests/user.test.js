538711e084a6c8b184ff36b8af7a0968688a890e
RA354347 <RA354347@wipro.com>
2019-04-21 00:19:03 +0530

    more assertions added to the user router test cases

--- tests/user.test.js
+++ tests/user.test.js
@@ -4,24 +4,24 @@ const jwt = require('jsonwebtoken');
 const app = require('../src/app');
 const User = require('../src/models/users');
 
-const userId = new mongoose.Types.ObjectId;
-const user = {
-    _id: userId,
+const userOneId = new mongoose.Types.ObjectId;
+const userOne = {
+    _id: userOneId,
     name: 'Max',
     email: 'max@test.com',
     password: 'max@12345',
     tokens: [{
-        token: jwt.sign({ _id: userId }, process.env.JWT_SECRET)
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
     }]
 }
 
 beforeEach(async () => {
     await User.deleteMany();
-    await new User(user).save();
+    await new User(userOne).save();
 });
 
 test('Should save a new user', async () => {
-    await request(app)
+    const response = await request(app)
         .post('/users')
         .send({
             name: 'Jane',
@@ -29,6 +29,22 @@ test('Should save a new user', async () => {
             password: 'jane@12345'
         })
         .expect(201);
+
+    // Assert if user got saved in the database successfully
+    const user = await User.findById(response.body.user._id);
+    expect(user).not.toBeNull();
+
+    // Assert the response
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'Jane',
+            email: 'jane@test.com'
+        },
+        token: user.tokens[0].token
+    });
+
+    // Assert the password is encrypted
+    expect(user.password).not.toBe('jane@12345');
 });
 
 test('Should not signup a user with existing email', async () => {
@@ -43,13 +59,17 @@ test('Should not signup a user with existing email', async () => {
 });
 
 test('Should login existing user', async () => {
-    await request(app)
+    const response = await request(app)
         .post('/users/login')
         .send({
-            email: user.email,
-            password: user.password
+            email: userOne.email,
+            password: userOne.password
         })
         .expect(200);
+
+    // Assert the new token generated and stored into the database
+    const user = await User.findById(userOneId);
+    expect(response.body.token).toBe(user.tokens[1].token);
 });
 
 test('Should not login non-existing user', async () => {
@@ -65,61 +85,91 @@ test('Should not login non-existing user', async () => {
 test('Should get user profile', async () => {
     await request(app)
         .get('/users/me')
-        .set('Authorization', `Bearer ${user.tokens[0].token}`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send({})
         .expect(200);
 });
 
 test('Should not get user profile for unauthenticated user', async () => {
-    await request(app)
+    const response = await request(app)
         .get('/users/me')
         .send({})
         .expect(401);
+
+    expect(response.body).toMatchObject({ 'error': 'Please authenticate' });
 });
 
 test('Should update user details', async () => {
-    await request(app)
+    const response = await request(app)
         .put('/users/me')
-        .set('Authorization', `Bearer ${user.tokens[0].token}`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send({
             name: 'Max S.',
             age: 26
         })
         .expect(200);
+    
+    const user = await User.findById(userOneId);
+    expect(response.body).toMatchObject({
+        name: 'Max S.',
+        age: 26
+    })
 });
 
 test('Should not update if invalid user details', async () => {
-    await request(app)
+    const response = await request(app)
         .put('/users/me')
-        .set('Authorization', `Bearer ${user.tokens[0].token}`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send({
             location: 'Bangalore',
             age: 26
         })
         .expect(400);
+
+    expect(response.body).toMatchObject({ 'error': 'Invalid update request.' });
+});
+
+test('Should not update for unauthenticated user', async () => {
+    const response = await request(app)
+        .put('/users/me')
+        .send({
+            name: 'Max S.',
+            age: 26
+        })
+        .expect(401);
+    
+    expect(response.body).toMatchObject({ error: 'Please authenticate' });
 });
 
 test('Should delete account for user', async () => {
     await request(app)
         .delete('/users/me')
-        .set('Authorization', `Bearer ${user.tokens[0].token}`)
-        .send({})
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
         .expect(200);
+
+    const user = await User.findById(userOneId);
+    // expect(user).toBeNull();
 });
 
 test('Should not delete unauthorized user', async () => {
-    await request(app)
-        .get('/users/me')
+    const response = await request(app)
+        .delete('/users/me')
         .send({})
         .expect(401);
+
+    expect(response.body).toMatchObject({ 'error': 'Please authenticate' });
 });
 
 test('Should upload profile image', async () => {
     await request(app)
         .post('/users/me/profileImage')
-        .set('Authorization', `Bearer ${user.tokens[0].token}`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .attach('file', 'tests/fixtures/default_profile_pic.jpg')
         .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.profileImage).toEqual(expect.any(Buffer));
 });
 
 test('Should not upload profile image for unauthorized user', async () => {
@@ -132,7 +182,7 @@ test('Should not upload profile image for unauthorized user', async () => {
 test('Should logout the current session for logged in user', async () => {
     await request(app)
         .post('/users/logout')
-        .set('Authorization', `Bearer ${user.tokens[0].token}`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .expect(200);
 });
 
@@ -145,7 +195,7 @@ test('Should not logout the current session for unauthorized user', async () => 
 test('Should logout all the sessions for logged in user', async () => {
     await request(app)
         .post('/users/logoutAll')
-        .set('Authorization', `Bearer ${user.tokens[0].token}`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .expect(200);
 });
 
